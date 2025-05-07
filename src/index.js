2d68ea2ed52c552b41ee038821013b16a7d07922
soeld0920 <w20040920@gmail.com>
2025-05-07 19:20:50 +0900

    3rd

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode basename={process.env.PUBLIC_URL}>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
